c0cb4426fe69b5751dae9ccdc158ebd3b391640b
ameyabankar <ameyabankar232009@gmail.com>
2026-08-21 00:13:40 -0700

    Add Practical 5 program 1: cube using inline function

---
+++ Practical-5/01_Cube_Inline_Function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+inline int cube(int n) {
+    return n * n * n;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Cube = " << cube(num) << endl;
+
+    return 0;
+}
