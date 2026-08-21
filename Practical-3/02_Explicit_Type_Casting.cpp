f3aad4508d1f86af0853bcd357a212505d0d33c1
ameyabankar <ameyabankar232009@gmail.com>
2026-08-21 00:12:00 -0700

    Add Practical 3 program 2: explicit type casting

---
+++ Practical-3/02_Explicit_Type_Casting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num = 12.75f;
+    int value;
+
+    value = (int)num; // Explicit type casting
+
+    cout << "Float value = " << num << endl;
+    cout << "Integer value = " << value << endl;
+
+    return 0;
+}
