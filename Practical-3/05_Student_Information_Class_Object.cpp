fc8d0e0ce112b97dc19cd3808ed115473caa873c
ameyabankar <ameyabankar232009@gmail.com>
2026-08-21 00:12:19 -0700

    Add Practical 3 program 5: student information

---
+++ Practical-3/05_Student_Information_Class_Object.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int rollNo;
+    string name;
+    float marks;
+
+public:
+    void getData() {
+        cout << "Enter roll number: ";
+        cin >> rollNo;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    void displayData() {
+        cout << "\nStudent Information" << endl;
+        cout << "Roll Number: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.getData();
+    s.displayData();
+
+    return 0;
+}
